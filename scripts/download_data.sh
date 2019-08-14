b96037d067d0326407afce1279600c8523494748
Mathias Müller <mathias.mueller@uzh.ch>
2019-08-14 11:39:57 +0200

    fix tar extraction

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -9,7 +9,7 @@ mkdir -p $data
 
 wget https://files.ifi.uzh.ch/cl/archiv/2019/clcontra/opus_robustness_data.tar.xz -P $data
 
-tar -xJfv $data/opus_robustness_data.tar.xz -C $data
+tar -xvf $data/opus_robustness_data.tar.xz -C $data
 
 # sizes
 echo "Sizes of corpora:"
