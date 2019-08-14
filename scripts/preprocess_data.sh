2e4265e3fa9036b483a0c0f4f447baf81ea75a24
Mathias Müller <mathias.mueller@uzh.ch>
2019-08-14 16:17:04 +0200

    sbatch mode for preprocessing

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 # work around slurm placing scripts in var folder
-if [[ $1 == "mode=sbatch"]]; then
+if [[ $1 == "mode=sbatch" ]]; then
   base=/net/cephfs/home/mathmu/scratch/domain-robustness
 else
   script_dir=`dirname "$0"`
   base=$script_dir/..
+fi;
 
 mkdir -p $base/shared_models
 
