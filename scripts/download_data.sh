cbcc6e43973b4ecd42143329e5b9ec4171c77976
Mathias Müller <mathias.mueller@uzh.ch>
2019-08-14 11:45:01 +0200

    wget check time stamp

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -7,7 +7,7 @@ data=$base/data
 
 mkdir -p $data
 
-wget https://files.ifi.uzh.ch/cl/archiv/2019/clcontra/opus_robustness_data.tar.xz -P $data
+wget -N https://files.ifi.uzh.ch/cl/archiv/2019/clcontra/opus_robustness_data.tar.xz -P $data
 
 tar -xvf $data/opus_robustness_data.tar.xz -C $data
 
