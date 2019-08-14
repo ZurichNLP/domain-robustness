4b06fe2766816475b836c7abe2e2f92ae4602142
Mathias Müller <mathias.mueller@uzh.ch>
2019-08-14 11:35:04 +0200

    executable, correct sockeye branch

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -10,3 +10,16 @@ mkdir -p $data
 wget https://files.ifi.uzh.ch/cl/archiv/2019/clcontra/opus_robustness_data.tar.xz -P $data
 
 tar -xJfv $data/opus_robustness_data.tar.xz -C $data
+
+# sizes
+echo "Sizes of corpora:"
+
+for domain in all it koran law medical subtitles; do
+    for corpus in train dev test; do
+      echo "corpus: "$domain/$corpus
+      wc -l $data/$domain/$corpus.de $data/$domain/$corpus.en
+    done
+done
+
+# sanity checks
+echo "At this point, please make sure that 1) number of lines are as expected, 2) language suffixes are correct and 3) files are parallel"
--- scripts/download_install_packages.sh
+++ scripts/download_install_packages.sh
@@ -13,9 +13,18 @@ echo "Make sure this script is executed AFTER you have activated a virtualenv"
 # CUDA version on instance
 CUDA_VERSION=100
 
-wget https://raw.githubusercontent.com/awslabs/sockeye/master/requirements/requirements.gpu-cu${CUDA_VERSION}.txt
-pip install sockeye --no-deps -r requirements.gpu-cu${CUDA_VERSION}.txt
-rm requirements.gpu-cu${CUDA_VERSION}.txt
+## Method A: install from PyPi
+
+# wget https://raw.githubusercontent.com/awslabs/sockeye/master/requirements/requirements.gpu-cu${CUDA_VERSION}.txt
+# pip install sockeye --no-deps -r requirements.gpu-cu${CUDA_VERSION}.txt
+# rm requirements.gpu-cu${CUDA_VERSION}.txt
+
+## Method B: install from Github repo (uncomment if that's what you need)
+
+git clone https://github.com/ZurichNLP/sockeye $tools/sockeye
+(cd $tools/sockeye && git checkout reconstruction_niu)
+
+pip install --no-deps -r $tools/sockeye/requirements/requirements.gpu-cu${CUDA_VERSION}.txt $tools/sockeye
 
 pip install matplotlib mxboard
 
