2977adc84a96cf7363b477cfa26febfb2f196ad2
Mathias Müller <mathias.mueller@uzh.ch>
2019-08-26 14:17:43 +0200

    generic distill script

---
+++ scripts/distillation/distill_generic.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+data=$base/data/$src-$trg
+scripts=$base/scripts
+
+distillations=$base/distillations
+mkdir -p $distillations
+
+distillations=$base/distillations/$src-$trg
+mkdir -p $distillations
+
+mkdir -p $distillations/$model_name
+
+MOSES=$base/tools/moses-scripts/scripts
+
+in_domain=medical
+
+num_threads=10
+
+OMP_NUM_THREADS=$num_threads python -m sockeye.translate \
+            -i $data/train.bpe.$src \
+            -o $distillations/$model_name/train.bpe.$model_name.$in_domain.$trg \
+            -m $base/models/$model_name \
+            --beam-size 10 \
+            --length-penalty-alpha 1.0 \
+            --device-ids 0 \
+            --batch-size 100 \
+            --disable-device-locking
+
+# no postprocessing needed for distillation
