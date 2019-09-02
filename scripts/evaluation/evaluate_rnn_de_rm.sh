6e6b84e2e6579dfd27bdb33d0a7e59ecf8799c3b
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-02 09:36:58 +0200

    rnn de rm eval

---
+++ scripts/evaluation/evaluate_rnn_de_rm.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# work around slurm placing scripts in var folder
+if [[ $1 == "mode=sbatch" ]]; then
+  base=/net/cephfs/home/mathmu/scratch/domain-robustness
+else
+  script_dir=`dirname "$0"`
+  base=$script_dir/../..
+fi;
+
+scripts=$base/scripts
+
+src=de
+trg=rm
+
+model_name=rnn
+
+domains="law blogs"
+
+. $scripts/evaluation/evaluate_generic.sh
