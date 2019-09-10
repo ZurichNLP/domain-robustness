abe152f02b9bf25114362f02f5ef41b512951740
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-10 09:45:25 +0200

    single device

---
+++ scripts/training/train_transformer_multilingual_single_gpu_de_en.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# work around slurm placing scripts in var folder
+if [[ $1 == "mode=sbatch" ]]; then
+  base=/rds/project/t2_vol4/rds-t2-cs037/mmueller/domain-robustness
+else
+  script_dir=`dirname "$0"`
+  base=$script_dir/../..
+fi;
+
+src=de
+trg=en
+
+data=$base/data/$src-$trg
+models=$base/models/$src-$trg
+scripts=$base/scripts
+
+mkdir -p $models
+
+model_name=transformer_multilingual
+
+mkdir -p $models/$model_name
+
+train_source=$data/medical/train.bpe.multilingual.$src
+train_target=$data/medical/train.bpe.multilingual.$trg
+
+dev_source=$data/medical/dev.bpe.multilingual.$src
+dev_target=$data/medical/dev.bpe.multilingual.$trg
+
+. $scripts/training/train_transformer_generic_single_gpu.sh
--- scripts/wrap-slurm-gpu-training-task-single-gpu.sh
+++ scripts/wrap-slurm-gpu-training-task-single-gpu.sh
@@ -7,7 +7,6 @@ module load rhel7/default-peta4
 module load cuda/9.1
 module add python-3.6.2-gcc-5.4.0-me5fsee
 
-
 # new
-sbatch -A T2-CS037-GPU --gres=gpu:2 --nodes=1 --time=36:00:00 --cpus-per-task 4 \
+sbatch -A T2-CS037-GPU --gres=gpu:1 --nodes=1 --time=36:00:00 --cpus-per-task 4 \
        -p pascal $1 mode=sbatch
