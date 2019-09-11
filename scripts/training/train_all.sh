5eb9cad024a0902056f5e9d6e645350886454d43
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-11 09:57:37 +0200

    fix bug in train all

--- scripts/training/train_all.sh
+++ scripts/training/train_all.sh
@@ -11,7 +11,7 @@ src=de
 for trg in en rm; do
   for model_name in transformer transformer_multilingual transformer_sentencepiece; do
 
-    if [[ ! -d $models/$src-$trg/model_name ]]; then
+    if [[ ! -d $models/$src-$trg/$model_name ]]; then
       $scripts/wrap-slurm-gpu-training-task.sh $scripts/training/train_${model_name}_${src}_${trg}.sh
     fi
   done
--- scripts/translation/translate_all.sh
+++ scripts/translation/translate_all.sh
@@ -12,7 +12,7 @@ src=de
 for trg in en rm; do
   for model_name in transformer transformer_multilingual transformer_sentencepiece transformer_reconstruction transformer_distillation; do
 
-    if [[ -d $models/$src-$trg/model_name ]]; then
+    if [[ -d $models/$src-$trg/$model_name ]]; then
       $scripts/wrap-slurm-gpu-translation-task.sh $scripts/translation/translate_${model_name}${src}_${trg}.sh
     fi
   done
