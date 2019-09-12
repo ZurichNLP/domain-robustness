7e00ea73ac4fd3a127f83fbf68e6bf70b7c4119d
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-12 09:27:44 +0200

    automate translate all

--- scripts/translation/translate_all.sh
+++ scripts/translation/translate_all.sh
@@ -13,7 +13,11 @@ for trg in en rm; do
   for model_name in transformer transformer_multilingual transformer_sentencepiece transformer_reconstruction transformer_distillation; do
 
     if [[ -d $models/$src-$trg/$model_name ]]; then
-      $scripts/wrap-slurm-gpu-translation-task.sh $scripts/translation/translate_${model_name}_${src}_${trg}.sh
+      echo "Model exists: $models/$src-$trg/$model_name"
+      if [[ ! -d $translations/$src-$trg/$model_name ]]; then
+        echo "Translations not found: $translations/$src-$trg/$model_name"
+        $scripts/wrap-slurm-gpu-translation-task.sh $scripts/translation/translate_${model_name}_${src}_${trg}.sh
+      fi
     fi
   done
 done
