bc03ce2ca4078b412ffd6fbc856fa4fa76500618
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-18 16:20:37 +0200

    translate multi+sr models

---
+++ scripts/translation/translate_transformer_multilingual+sentencepiece_de_en.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# work around slurm placing scripts in var folder
+if [[ $1 == "mode=sbatch" ]]; then
+  base=/rds/project/t2_vol4/rds-t2-cs037/mmueller/domain-robustness
+else
+  script_dir=`dirname "$0"`
+  base=$script_dir/../..
+fi;
+
+scripts=$base/scripts
+
+src=de
+trg=en
+
+model_name=transformer_multilingual+sentencepiece
+
+in_domain=medical
+
+domains="it koran law medical subtitles"
+
+. $scripts/translation/translate_transformer_multilingual+sentencepiece_generic.sh
---
+++ scripts/translation/translate_transformer_multilingual+sentencepiece_de_rm.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# work around slurm placing scripts in var folder
+if [[ $1 == "mode=sbatch" ]]; then
+  base=/rds/project/t2_vol4/rds-t2-cs037/mmueller/domain-robustness
+else
+  script_dir=`dirname "$0"`
+  base=$script_dir/../..
+fi;
+
+scripts=$base/scripts
+
+src=de
+trg=rm
+
+model_name=transformer_multilingual+sentencepiece
+
+in_domain=law
+
+domains="law blogs"
+
+. $scripts/translation/translate_transformer_multilingual+sentencepiece_generic.sh
