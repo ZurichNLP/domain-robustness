e382092e759955c126325d0d5e70866472d55f88
Mathias Müller <mathias.mueller@uzh.ch>
2019-10-11 14:54:39 +0200

    generic translate with custom model

---
+++ scripts/translation/translate_generic_custom_model.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+data=$base/data/$src-$trg
+scripts=$base/scripts
+
+translations=$base/translations
+mkdir -p $translations
+
+translations=$base/translations/$src-$trg
+mkdir -p $translations
+
+mkdir -p $translations/$model_name
+
+MOSES=$base/tools/moses-scripts/scripts
+
+if [[ "$CUDA_VISIBLE_DEVICES" == "NoDevFiles" ]]; then
+  num_threads=64
+  device_arg="--use-cpu"
+else
+  num_threads=3
+  device_arg="--device-ids 0"
+fi
+
+for domain in $domains; do
+
+    data=$base/data/$src-$trg
+
+    if [[ $domain != $in_domain ]]; then
+      data=$data/$domain/test_unknown_domain/$in_domain
+    else
+      data=$data/$domain
+    fi
+
+    OMP_NUM_THREADS=$num_threads python -m sockeye.translate \
+            -i $data/test.bpe.$src \
+            -o $translations/$model_name/test.bpe.$model_name.$domain.$trg \
+            -m $model_path \
+            --beam-size 10 \
+            --length-penalty-alpha 1.0 \
+            $device_arg \
+            --batch-size 64 \
+            --disable-device-locking
+
+    # undo BPE
+
+    cat $translations/$model_name/test.bpe.$model_name.$domain.$trg | sed -r 's/@@( |$)//g' > $translations/$model_name/test.truecased.$model_name.$domain.$trg
+
+    # undo truecasing
+
+    cat $translations/$model_name/test.truecased.$model_name.$domain.$trg | $MOSES/recaser/detruecase.perl > $translations/$model_name/test.tokenized.$model_name.$domain.$trg
+
+    # undo tokenization
+
+    cat $translations/$model_name/test.tokenized.$model_name.$domain.$trg | $MOSES/tokenizer/detokenizer.perl -l $trg > $translations/$model_name/test.$model_name.$domain.$trg
+
+done
