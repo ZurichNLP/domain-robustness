17ff1cda61aae4c8fc5e4b611afca411fa55d5d0
Mathias Müller <mathias.mueller@uzh.ch>
2019-10-29 14:30:56 +0100

    conditional nbest reranked eval

--- scripts/evaluation/evaluate_generic.sh
+++ scripts/evaluation/evaluate_generic.sh
@@ -28,26 +28,34 @@ for domain in $domains; do
 
 done
 
-bleu_reranked=$base/bleu_reranked
-mkdir -p $bleu_reranked
-
-bleu_reranked=$base/bleu_reranked/$src-$trg
-mkdir -p $bleu_reranked
-
-mkdir -p $bleu_reranked/$model_name
-
 echo "######################################################"
 echo "RERANKED"
 
-for domain in $domains; do
+if [[ -d $scores/$model_name ]]; then
 
-    data=$base/data/$src-$trg/$domain
+  bleu_reranked=$base/bleu_reranked
+  mkdir -p $bleu_reranked
 
-    # compute case-sensitive BLEU on detokenized data
+  bleu_reranked=$base/bleu_reranked/$src-$trg
+  mkdir -p $bleu_reranked
 
-    cat $scores/$model_name/test.reranked_best.$model_name.$domain.$trg | sacrebleu $data/test.$trg > $bleu_reranked/$model_name/test.reranked_best.$model_name.$domain.bleu
+  mkdir -p $bleu_reranked/$model_name
 
-    echo "$bleu_reranked/$model_name/test.$model_name.$domain.bleu"
-    cat $bleu_reranked/$model_name/test.$model_name.$domain.bleu
+  for domain in $domains; do
 
-done
+      data=$base/data/$src-$trg/$domain
+
+      # compute case-sensitive BLEU on detokenized data
+
+      cat $scores/$model_name/test.reranked_best.$model_name.$domain.$trg | sacrebleu $data/test.$trg > $bleu_reranked/$model_name/test.reranked_best.$model_name.$domain.bleu
+
+      echo "$bleu_reranked/$model_name/test.$model_name.$domain.bleu"
+      cat $bleu_reranked/$model_name/test.$model_name.$domain.bleu
+
+  done
+
+else
+
+  echo "Reranked best translations (in $scores/$model_name) do not seem to exist."
+
+fi;
